39f80863c59b28764530f69c4b513881c5e21943
davedavehong <davedavehong@qq.com>
2015-09-12 12:08:56 +0800

    基本的静态页面

--- javascript/home.js
+++ javascript/home.js
@@ -4,8 +4,11 @@
 
 $(function () {
     var t = null;
+    var itemShowT =null;
     var headerLogo = $('a.logo');
     var snsNav = $('.sns-nav');
+    var player = $('#player');
+    var itemShow = $('#items-preview');
     headerLogo.mouseenter(function () {
         clearTimeout(t);
         snsNav.addClass('sns-show');
@@ -24,4 +27,20 @@ $(function () {
         $(this).removeClass("hover").removeClass('sns-show');
     });
     $('a').attr('target','_blank');
+    player.mouseenter(function () {
+        itemShow.addClass('show');
+    });
+    player.mouseleave(function () {
+        itemShowT =setTimeout(function () {
+            if(!itemShow.hasClass('hover')){
+                itemShow.removeClass('show')
+            }
+        },200);
+    });
+    itemShow.mouseenter(function () {
+        $(this).addClass('hover')
+    });
+    itemShow.mouseleave(function () {
+        $(this).removeClass("hover").removeClass('show')
+    })
 });
