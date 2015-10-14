3afe56e6135d9cad37312fb6c45e8be88dd6a2c1
davedavehong <davedavehong@qq.com>
2015-10-14 10:14:04 +0800

    修正IE10及以下浏览器的图片自带黑框问题

--- javascript/home.js
+++ javascript/home.js
@@ -4,11 +4,27 @@
 
 $(function () {
     var t = null;
-    var itemShowT =null;
+    var itemShowT = null;
     var headerLogo = $('a.logo');
     var snsNav = $('.sns-nav');
     var player = $('#player');
     var itemShow = $('#items-preview');
+    var post = $('#post-collections');
+    var ee = $('#ee');
+
+    post.children('div').each(function () {
+        $(this).mouseover(function () {
+            post.children('div').height(50).css({
+                'background-color': '#591f27',
+                'color': 'rgba(255,255,255,0.56)'
+            });
+            $(this).height(100).css({
+                'color':'#fff',
+                'background-color':'rgba(25, 55, 255, 0.34)'
+            });
+        });
+    });
+
     headerLogo.mouseenter(function () {
         clearTimeout(t);
         snsNav.addClass('sns-show');
@@ -26,16 +42,16 @@ $(function () {
     snsNav.mouseleave(function () {
         $(this).removeClass("hover").removeClass('sns-show');
     });
-    $('a').attr('target','_blank');
+    $('a').attr('target', '_blank');
     player.mouseenter(function () {
         itemShow.addClass('show');
     });
     player.mouseleave(function () {
-        itemShowT =setTimeout(function () {
-            if(!itemShow.hasClass('hover')){
+        itemShowT = setTimeout(function () {
+            if (!itemShow.hasClass('hover')) {
                 itemShow.removeClass('show')
             }
-        },200);
+        }, 200);
     });
     itemShow.mouseenter(function () {
         $(this).addClass('hover')
